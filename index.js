8d1826c67bd2c1dbe74768e734080e8d926d4be6
Szymon Sakowicz <giebeka@gmail.com>
2018-10-28 10:52:19 +0100

    removed unused components

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, View} from 'react-native';
+import {AppRegistry, View} from 'react-native';
 
-import Statuses from './app/components/Statuses/Statuses';
 import TextField from './app/components/TextField/TextField';
 
 export default class TrackingApp extends Component {
     render() {
       return (
-        <View>
+        <View>z
             <TextField/>
         </View>
       );
